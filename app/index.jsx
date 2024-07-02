f14fb79bdae81b406c8da342e18aedbabfe55128
Thomas Lindop <thomas16256@gmail.com>
2024-07-02 12:05:28 +0100

    >> add button which triggers an animation > also changes some dummy state

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,27 @@
+import { Suspense, useState, } from 'react';
+import { View, Text, Button, Stylesheet, } from 'react-native';
 import { Canvas } from '@react-three/fiber/native';
 import useControls from 'r3f-native-orbitcontrols';
-import { Suspense } from 'react';
+import { animated, useSpring, } from '@react-spring/native';
 import Crisp from '../components/3dModel/Crisp';
 
 const Home = () => {
   const [OrbitControls, events] = useControls();
-  return (
+  const [fooState, setFooState] = useState(false);
+  
+  function toggleFooState() {
+    setFooState((currentFooState) => !currentFooState);
+    springsApi.start({
+      from: { width: 50 },
+      to: { width: 200 },
+    });
+  }
+  
+  const [springs, springsApi] = useSpring(() => ({
+    from: { width: 50 },
+  }));
+
+  return (<>
     <Canvas>
       <OrbitControls enablePan={false} enableZoom={false} />
       <directionalLight position={[1, 0, 0]} args={['white', 2]} />
@@ -18,7 +34,18 @@ const Home = () => {
         <Crisp />
       </Suspense>
     </Canvas>
-  );
+    <View>
+      <animated.View style={{
+        width: 50,
+        height: 50,
+        backgroundColor:  'red',
+        ...springs,
+      }}>
+      </animated.View>
+      <Button onPress={toggleFooState} title="Click me! I do things." color="blue" />
+      <Text>{fooState ? 'foo!' : 'no foo!'}</Text>
+    </View>
+  </>);
 };
 
 export default Home;
