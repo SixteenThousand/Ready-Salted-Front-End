edcb5536ecc084e7b154507944832cc5fa905609
lucMarteau <lucmarteau@gmail.com>
2024-07-03 15:30:19 +0100

    minor change with no effect

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,32 @@
-import useControls from 'r3f-native-orbitcontrols';
-import HomeScreen from '../screens/home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import SignupScreen from '../screens/signup';
-import LoginScreen from '../screens/login';
-import TitleScreen from '../screens/titlescreen';
+import useControls from "r3f-native-orbitcontrols";
+import HomeScreen from "../screens/home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignupScreen from "../screens/signup";
+import LoginScreen from "../screens/login";
+import TitleScreen from "../screens/titlescreen";
 
-
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const Home = () => {
   const [OrbitControls, events] = useControls();
   return (
-   <NavigationContainer independent={true} >
-
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name='signup' component={SignupScreen} options={{ headerShown: true }}/>
-        <Stack.Screen name='login' component={LoginScreen}/>
-        <Stack.Screen name='title' component={TitleScreen}/>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="signup"
+          component={SignupScreen}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="title" component={TitleScreen} />
       </Stack.Navigator>
-   </NavigationContainer>
+    </NavigationContainer>
   );
 };
 
