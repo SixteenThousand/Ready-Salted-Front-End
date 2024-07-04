9843b0c2d95b9a5672a008ed83a3ba3966d14fbb
Thomas Lindop <thomas16256@gmail.com>
2024-07-04 13:59:45 +0100

    >> remove crisp enlargment & spinning animations

--- components/3dModel/Crisp.jsx
+++ components/3dModel/Crisp.jsx
@@ -1,31 +1,10 @@
 import { useGLTF } from '@react-three/drei/native';
-import { useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { useSpring, animated, config } from '@react-spring/three';
 
 function Model() {
   const { scene } = useGLTF(require('../../assets/models/crisp.glb'));
-  const modelRef = useRef();
   
-  // react-spring stuff
-  const [active,setActive] = useState(false);
-  const { scale } = useSpring({
-    scale: active ? 2 : 1,
-    config: config.wobbly,
-  });
-  
-  
-  useFrame(({ clock }) => {
-    modelRef.current.rotation.y = clock.getElapsedTime();
-  });
-  
-  return <animated.primitive
+  return <primitive
     object={scene}
-    ref={modelRef}
-    scale={scale}
-    onClick={() => {
-      setActive((currentlyActive) => !currentlyActive);
-    }}
   />;
 }
 
