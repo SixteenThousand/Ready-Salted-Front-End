d55714845aba3b6cad95ce24f360fb609663d5ec
lucMarteau <lucmarteau@gmail.com>
2024-07-08 13:51:00 +0100

    Got a sound working on userprofile

--- screens/userprofile.jsx
+++ screens/userprofile.jsx
@@ -1,26 +1,54 @@
+import React, { useState } from "react";
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
   ImageBackground,
 } from "react-native";
-import React from "react";
+import { Audio } from "expo-av";
 import styles from "../styles";
 
+const BoopButton = () => {
+  const [sound, setSound] = useState();
 
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('../assets/MP3/T. Bless - Froggy Fraud Adventure.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); 
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <TouchableOpacity onPress={playSound} style={styles.button}>
+      <Text style={styles.buttonText}>Music!</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function ScoreScreen() {
   return (
     <ImageBackground
       style={styles.titleImage}
       source={require("../assets/images/Designer-6.jpg")}
-
       resizeMode="cover"
     >
-    <View style={styles.container}>
-         <Text>HIGH SCORES</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>HIGH SCORES</Text>
+        <BoopButton />
+      </View>
     </ImageBackground>
   );
 }
