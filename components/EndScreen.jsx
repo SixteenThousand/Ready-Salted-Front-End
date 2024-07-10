817d4006b5ffddfff02c6fe3bf68bfc7cdddf2b8
Thomas Lindop <thomas16256@gmail.com>
2024-07-10 12:37:11 +0100

    >> add styling & content to end screen

--- components/EndScreen.jsx
+++ components/EndScreen.jsx
@@ -1,5 +1,52 @@
-import { View, Text, } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native';
+import globalStyleSheet from '../styles'; 
 
-export default function EndScreen({ setIsGameOver }) {
-  return <View><Text>Game Over!</Text></View>;
+export default function EndScreen({ setIsGameOver, score }) {
+  const restartGame = () => {
+    setIsGameOver(false);
+  };
+  const exitGame = () => {
+    // don't know what to do here yet
+  };
+
+  return <View style={styles.allContainer}>
+    <Text style={styles.endMessage}> Game Over! </Text>
+    <Text style={styles.scoreMessage}>Your score was:</Text>
+    <Text style={styles.scoreNumber}>{score}</Text>
+    <View style={styles.buttonsContainer}>
+      <TouchableOpacity style={globalStyleSheet.Button2} onPress={restartGame}>
+        <Text style={styles.buttonText}>New Game</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={globalStyleSheet.Button2} onPress={exitGame}>
+        <Text style={styles.buttonText}>Exit</Text>
+      </TouchableOpacity>
+    </View>
+  </View>;
 }
+
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  allContainer: {
+    backgroundColor: 0x00aaaabb,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 24,
+  },
+  endMessage: {
+    fontSize: 36,
+  },
+  scoreMessage: {
+    fontSize: 24,
+  },
+  scoreNumber: {
+    fontSize: 96,
+    fontWeight: 'bold',
+  },
+});
